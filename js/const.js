9f98f25daa0b8a0d9d0ee41678199516a0f00423
nonameno01 <143680425+nonameno01@users.noreply.github.com>
2023-12-03 18:50:50 +0900

    Update const.js

--- js/const.js
+++ js/const.js
@@ -2,4 +2,4 @@
 // WSURL = 'wss://cataso2.omatz.cf';
 // WSURL = 'wss://cataso-omatz.herokuapp.com';
 // WSURL = 'ws://localhost:5000';
-WSURL = 'wss://cataso-server2.onrender.com';
+WSURL = 'https://cataso-server-2lmg.onrender.com';
